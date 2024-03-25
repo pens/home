bcc57b4f7a2bc9d720b18ef133f64a77d9fe31dd
Seth Pendergrass <seth.pendergrass@bytedance.com>
2024-03-25 10:54:00 -0700

    Better comment

--- .config/powershell/profile.ps1
+++ .config/powershell/profile.ps1
@@ -1,5 +1,5 @@
 # Public PowerShell Profile, for $PROFILE.CurrentUserAllHosts.
-# Note: Use separate $PROFILE.AllUserAllHosts profile for private / work-specific configuration.
+# Note: Use separate $PROFILE.CurrentUserCurrentHost profile for private / work-specific configuration.
 # See https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.4
 
 Set-Alias -Name vi -Value nvim
